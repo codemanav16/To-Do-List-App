9dced3626e8841d24073788d8c8294094c09cff3
codemanav16 <manavanal16@gmail.com>
2026-02-24 00:20:21 +0530

    file added

---
+++ script.js
@@ -0,0 +1,41 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === '') {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+
+    // Add delete button
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"; // × symbol
+    li.appendChild(span);
+
+    listContainer.appendChild(li);
+    inputBox.value = ""; 
+    saveData()
+  }
+}
+
+// Event delegation for checking & deleting
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data",listContainer.innerHTML);
+
+}
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
